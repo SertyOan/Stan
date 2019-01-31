83067849c03405c0e2d778deb66ffd97e7e92375
SertyOan <serty.oan@osyra.net>
2019-01-31 23:17:31 +0100

    add helpers class with params checking

---
+++ server/php/App/Helpers.php
@@ -0,0 +1,26 @@
+<?php
+namespace App;
+
+class Helpers {
+    public static function checkParams($params, $field, $function) {
+        if(!is_object($params)) {
+            throw new \InvalidArgumentException('Object expected');
+        }
+
+        if(!is_string($field) || !preg_match('/^[a-z]+$/i', $field)) {
+            throw new \InvalidArgumentException('Invalid field');
+        }
+
+        if(!property_exists($params, $field)) {
+            throw new \InvalidArgumentException('Parameter "categoryID" expected');
+        }
+
+        if(function_exists($function)) {
+            throw new \InvalidArgumentException('Invalid function');
+        }
+
+        if(!call_user_function($function, $params->{$field})) {
+            throw new \InvalidArgumentException('Parameter "'.$field.'" is invalid');
+        }
+    }
+}
