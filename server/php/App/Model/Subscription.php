499fe97651a646a23e18ad3858b45325e95e9921
SertyOan <serty.oan@osyra.net>
2019-02-18 20:52:42 +0100

    renaming field

--- server/php/App/Model/Subscription.php
+++ server/php/App/Model/Subscription.php
@@ -7,20 +7,19 @@ class Subscription extends Object {
     const
         DATABASE_CLASS = '\\App\\Database',
         DATABASE_SCHEMA = STAN_DATABASE_SCHEMA,
-        DATABASE_TABLE = 'Subscription',
-        ROLE_OWNER = 1;
+        DATABASE_TABLE = 'Subscription';
 
 	protected static
 		$properties = Array(
 			'id' => Array('class' => 'Integer'),
 			'user' => Array('class' => '\\App\\Model\\User'),
 			'category' => Array('class' => '\\App\\Model\\Category'),
-            'role' => Array('class' => 'Integer')
+            'owner' => Array('class' => 'Integer')
 		);
 
     protected
         $id,
         $user,
         $category,
-        $role;
+        $owner;
 }
